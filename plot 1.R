073759e6b3911ec7970371029f078b91256e7832
Hyungue Lim <40773528+limberly@users.noreply.github.com>
2019-08-20 11:36:25 -0400

    Final Commit for Submission

---
+++ plot 1.R
@@ -0,0 +1,26 @@
+#read data
+a <- read.table("~/Documents/Coursera Data Science/Exploratory Analysis/household_power_consumption.txt", header = TRUE, sep=";")
+
+#combine date and time
+a$Date_Time <- paste(a$Date, a$Time)
+
+#convert date and time into POSIXct
+a$Date_Time <- as.POSIXct(a$Date_Time, format = "%d/%m/%Y %H:%M:%S")
+
+#convert date into POSIXct
+a$Date <- as.POSIXct(a$Date, format = "%d/%m/%Y")
+
+#select values for the needed dates
+b <- a[which(a$Date == "2007-02-01" | a$Date == "2007-02-02"),]
+
+#convert variables into numeric because they are factors
+b[,3:9] <- sapply(b[,3:9], as.character)
+b[,3:9] <- sapply(b[,3:9], as.numeric)
+
+#create plot
+png("plot1.png", width = 480, height = 480)
+par(mar=c(4,4,6,2), pin = c(3,4))
+hist(b$Global_active_power, col = "red", xlab = "Global Active Power (kilowatts)", main = "Global Active Power")
+dev.off()
+
+
---
+++ plot 4.R
@@ -0,0 +1,38 @@
+#read data
+a <- read.table("~/Documents/Coursera Data Science/Exploratory Analysis/household_power_consumption.txt", header = TRUE, sep=";")
+
+#combine date and time
+a$Date_Time <- paste(a$Date, a$Time)
+
+#convert date and time into POSIXct
+a$Date_Time <- as.POSIXct(a$Date_Time, format = "%d/%m/%Y %H:%M:%S")
+
+#convert date into POSIXct
+a$Date <- as.POSIXct(a$Date, format = "%d/%m/%Y")
+
+#select values for the needed dates
+b <- a[which(a$Date == "2007-02-01" | a$Date == "2007-02-02"),]
+
+#convert variables into numeric because they are factors
+b[,3:9] <- sapply(b[,3:9], as.character)
+b[,3:9] <- sapply(b[,3:9], as.numeric)
+
+#create plot
+png("plot4.png", width = 480, height = 480)
+par(mfcol=c(2,2))
+plot(x= b$Date_Time, y= b$Global_active_power, col = "black", ylab = "Global Active Power (kilowatts)", xlab = "", type= "n")
+lines(x= b$Date_Time, y= b$Global_active_power, type = "l")
+
+plot(x= b$Date_Time, y= b$Sub_metering_1 , col = "black", ylab = "Global Active Power (kilowatts)", xlab = "", type= "n")
+lines(x= b$Date_Time, y= b$Sub_metering_1 , type = "l")
+lines(x= b$Date_Time, y= b$Sub_metering_2 , type = "l", col = "red")
+lines(x= b$Date_Time, y= b$Sub_metering_3 , type = "l", col = "blue")
+legend(x="topright", legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), col = c("black", "red", "blue"), lty = "solid")
+
+plot(x= b$Date_Time, y= b$Voltage, col = "black", ylab = "Voltage", xlab = "datetime", type= "n")
+lines(x= b$Date_Time, y= b$Voltage, type = "l")
+
+plot(x= b$Date_Time, y= b$Global_reactive_power, col = "black", ylab = "Global_reactive_power", xlab = "datetime", type= "n")
+lines(x= b$Date_Time, y= b$Global_reactive_power, type = "l")
+
+dev.off()
